c89e61cb265fd0cbe7336f2edbabe3ae984b4391
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-02-03 00:21:43 +0330

    Add Lists
    -Adds visited to save visit status
    -Adds ListOfVertices to save order of vertex traversal
    -Adds SSC to save components

--- Kosaraju.py
+++ Kosaraju.py
@@ -1,2 +1,5 @@
 # Kusaraju's Algorithm to find Strongly Connected Componnents(SCC) using a DFS approach 
 
+visited = {} 
+ListOfVerteces = [] # An ordered list of vertices
+SSC = {} # dictinary of (key = vertex ,value = rootVertex) values
