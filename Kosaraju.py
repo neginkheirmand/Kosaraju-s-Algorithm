cd7d3ab4989f0ab0cbcdb3bc606f0f778df0fbf2
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-02-03 00:39:58 +0330

    Create Graph
    -Adds graph dictionary to save edges between vetices
    -Adds addEdge function
    -Adds getNeighbors function

--- Kosaraju.py
+++ Kosaraju.py
@@ -1,5 +1,19 @@
 # Kusaraju's Algorithm to find Strongly Connected Componnents(SCC) using a DFS approach 
 
+
+
 visited = {} 
 ListOfVerteces = [] # An ordered list of vertices
 SSC = {} # dictinary of (key = vertex ,value = rootVertex) values
+
+graph = {}
+
+def addEdge(v1 , v2):
+    if graph.get(v1) is None:
+        graph[v1] = [v2]
+    else:
+        graph[v1].append(v2)
+
+def getNeighbors(v):
+    return graph[v]
+    
